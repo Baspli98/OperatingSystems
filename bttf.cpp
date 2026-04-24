c60aafb7f0ef1fcd1f7eba3545f0ae88c718f1b6
Baspli98 <leonkluemper@hotmail.de>
2026-04-25 00:54:10 +0200

    Implemented back to the future functions for FluxCapacitor. Implemented template function for BubbleSort. Git init.

---
+++ bttf.cpp
@@ -0,0 +1,56 @@
+#include "bttf.h"
+#include <cstdlib>
+#include <ctime>
+#include "sort.h"
+
+namespace bttf {
+    FluxCapacitor* createFluxCapacitor() {
+        FluxCapacitor* fc = new FluxCapacitor;
+
+        //c_string is an char* so,
+        //... first, determine a random length for the name (range between 6 and 10)
+        int length = std::rand() % 6 + 5;
+        //... second, assign the length to a new char*
+        fc->c_string = new unsigned char[length + 1];
+        //... third, use pointer arithmetic to fill the places
+        for(int i = 0; i < length; i++) {
+            //... fourth, in the ASCII table letters go from 65 to 90
+            //use modulo 26 with rand
+            fc->c_string[i] = 'A' + (std::rand() % 26);
+        }
+
+        fc->c_string[length] = '\0';
+
+        // Generating a positive non negative random number
+        fc->value = std::rand() % 100;
+
+        return fc;
+    }
+
+    FluxCapacitor** createFluxCapacitorArray(unsigned int n) {
+        FluxCapacitor** array = new FluxCapacitor*[n];
+
+        for(int i = 0; i < n; i++) {
+            array[i] = createFluxCapacitor();
+        }
+        std::cout << "FluxCapacitorArray generated." << std::endl;
+        return array;
+    }
+
+    void printFluxCapacitorArray(FluxCapacitor** array, unsigned int n) {
+        std::cout << "----------START PRINTING FluxCapacitorArray----------" << std::endl;
+        for(int i = 0; i < n; i++) {
+            std::cout << "Array Index: " << i << std::endl;
+            std::cout << "Name: " << array[i]->c_string << std::endl;
+            std::cout << "Value: " << array[i]->value << std::endl;
+        }
+        std::cout << "----------END PRINTING FluxCapacitorArray------------" << std::endl;
+    }
+
+    void bubbleSortFluxCapacitorArray(FluxCapacitor** array, unsigned int n) {
+        sort::bubbleSort(array, n, [](FluxCapacitor* a, FluxCapacitor* b){
+            return a->value > b->value;
+        });
+        std::cout << "FluxCapacitorArray sorted." << std::endl;
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "bttf.h"
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void doAssignment_1(bool doit);
+
+int main()
+{
+    doAssignment_1(true);
+    cout << "---------That's all folks!---------" << endl;
+    return 0;
+}
+
+void doAssignment_1(bool doit) {
+    if(doit) {
+        std::srand(std::time(nullptr));
+        int fc_amount = 10;
+        auto array = bttf::createFluxCapacitorArray(fc_amount);
+        bttf::printFluxCapacitorArray(array, fc_amount);
+        bttf::bubbleSortFluxCapacitorArray(array, fc_amount);
+        bttf::printFluxCapacitorArray(array, fc_amount);
+    }
+}
