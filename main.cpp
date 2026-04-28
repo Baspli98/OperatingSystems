1acd062c63d7ac2c9313501c5e373aea6aec4ddb
Baspli98 <leonkluemper@hotmail.de>
2026-04-28 23:44:56 +0200

    Added Thread Functionalities

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "container.h"
 #include <cstdlib>
 #include <ctime>
+#include <pthread.h>
 
 using namespace std;
 
@@ -11,6 +12,8 @@ void doAssignment_2(bool doit);
 
 struct bttf::DeLorean* delorean__;
 struct bttf::FluxCapacitor** capacitors__;
+
+pthread_mutex_t mutex;
 
 int main()
 {
@@ -33,6 +36,8 @@ void doAssignment_1(bool doit) {
 }
 
 void* assembleDeLorean(void *indices_) {
+    pthread_mutex_lock(&mutex);
+
     container::IndexContainer* idx = static_cast<container::IndexContainer*>(indices_);
     unsigned int sum = 0;
 
@@ -43,18 +48,26 @@ void* assembleDeLorean(void *indices_) {
 
     delorean__->current_power_level_in_mega_watts += sum;
 
-    std::cout << "IndexContainer added" << std::endl;
+    std::cout << "Thread sum added: " << sum << std::endl;
+
+    pthread_mutex_unlock(&mutex);
     return nullptr;
 }
 
 void doAssignment_2(bool doit) {
     if(doit) {
+        pthread_mutex_init(&mutex, nullptr);
+
         unsigned int capacitorAmount = 121;
         unsigned int capacitorValue = 10;
 
         capacitors__ = bttf::createFluxCapacitorArray(capacitorAmount, capacitorValue);
         //bttf::printFluxCapacitorArray(capacitors__, capacitorAmount);
         delorean__ = new bttf::DeLorean;
+        delorean__->current_power_level_in_mega_watts = 0;
+        delorean__->speed_in_kmh = 0;
+
+        //Create IndexContainer
         auto c1 = new container::IndexContainer;
         c1->array_length = 21;
         c1->cap_indices = new unsigned int[21];
@@ -68,9 +81,21 @@ void doAssignment_2(bool doit) {
             c2->cap_indices[i] = i + 21;
         }
 
-        assembleDeLorean(c1);
-        assembleDeLorean(c2);
+        //Function Call one Thread
+        //assembleDeLorean(c1);
+        //assembleDeLorean(c2);
+
+        //Threads
+        pthread_t t1, t2;
+
+        pthread_create(&t1, nullptr, assembleDeLorean, c1);
+        pthread_create(&t2, nullptr, assembleDeLorean, c2);
+
+        pthread_join(t1, nullptr);
+        pthread_join(t2, nullptr);
 
         std::cout << delorean__->current_power_level_in_mega_watts << std::endl;
+
+        pthread_mutex_destroy(&mutex);
     }
 }
