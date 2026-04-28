7440bbbf9c033ec5116aacec8538a6932707b080
Baspli98 <leonkluemper@hotmail.de>
2026-04-28 23:11:23 +0200

    Implemented assembleDelorean Function which takes a void pointer (in this case IndexContainer) that only adds the sum of a specific range of FluxCapacitors value attribute

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "bttf.h"
+#include "container.h"
 #include <cstdlib>
 #include <ctime>
 
@@ -31,6 +32,21 @@ void doAssignment_1(bool doit) {
     }
 }
 
+void* assembleDeLorean(void *indices_) {
+    container::IndexContainer* idx = static_cast<container::IndexContainer*>(indices_);
+    unsigned int sum = 0;
+
+    for (int i = 0; i < idx->array_length; i++) {
+        //std::cout << idx->cap_indices[i] << std::endl;
+        sum += capacitors__[idx->cap_indices[i]]->value;
+    }
+
+    delorean__->current_power_level_in_mega_watts += sum;
+
+    std::cout << "IndexContainer added" << std::endl;
+    return nullptr;
+}
+
 void doAssignment_2(bool doit) {
     if(doit) {
         unsigned int capacitorAmount = 121;
@@ -39,5 +55,22 @@ void doAssignment_2(bool doit) {
         capacitors__ = bttf::createFluxCapacitorArray(capacitorAmount, capacitorValue);
         //bttf::printFluxCapacitorArray(capacitors__, capacitorAmount);
         delorean__ = new bttf::DeLorean;
+        auto c1 = new container::IndexContainer;
+        c1->array_length = 21;
+        c1->cap_indices = new unsigned int[21];
+        for (int i = 0; i < c1->array_length; i++) {
+            c1->cap_indices[i] = i;
+        }
+        auto c2 = new container::IndexContainer;
+        c2->array_length = 100;
+        c2->cap_indices = new unsigned int[100];
+        for (int i = 0; i < c2->array_length; i++) {
+            c2->cap_indices[i] = i + 21;
+        }
+
+        assembleDeLorean(c1);
+        assembleDeLorean(c2);
+
+        std::cout << delorean__->current_power_level_in_mega_watts << std::endl;
     }
 }
